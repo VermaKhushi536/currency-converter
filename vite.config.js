ab95921aaf9333a193f569d6529c927aa10d7599
Khushi <sushmit@Sushmits-MacBook-Air.local>
2026-05-21 16:50:16 +0530

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  
+  base: '/currency-converter/',
   plugins: [
     tailwindcss(),
   ],
